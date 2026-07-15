7f8725509365a4cf9d3eb025d0bf9e8e0f43b209
Divyanshu-hehe <divyanshupandey4338@gmail.com>
2026-07-15 15:32:51 +0530

    practise

---
+++ loops/pattern1.java
@@ -0,0 +1,15 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        int n=4;
+        int m=4;
+
+    for(int i=0;i<n;i++){
+            for(int j=i;j<4;j++)
+         System.out.print("*");
+             
+            System.out.println();
+        }
+          
+    }
+    
+}
